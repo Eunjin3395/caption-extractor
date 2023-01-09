bd2705911e8e39298c86fc250f33f2ba84731578
Eunjin <jennyeunjin@khu.ac.kr>
2023-01-09 16:39:11 +0900

    add gitignore & update auto.py

--- auto.py
+++ auto.py
@@ -1,21 +1,18 @@
-fileName="7주차2"
+fileName="{}주차{}".format(13,1)
 vttFile=open(fileName+".txt","rt")
-
 outFile=open(fileName+"_수정.hwp","w",encoding="cp949")
-
 outLine=""
 while True:
-  line=vttFile.readline()
-  if not line: break
-  if("-->" not in line):
-    tempLine=line.replace("\n"," ")
+      line=vttFile.readline()
+      if not line: break
+      if("-->" not in line):
+        tempLine=line.replace("\n"," ")
 
-    outLine+=tempLine
+        outLine+=tempLine
 
 
 outFile.write(outLine)
 
-
 vttFile.close()
 outFile.close()
-print("success")
+print("{} success".format(fileName))
